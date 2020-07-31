62934762228e3802d6a65a98ae636a08e2501bfa
sokunev <okunevso@inbox.ru>
2020-07-31 17:59:13 +0400

    add sl4j

--- src/main/java/com/example/LogarithmResolver/controllers/LogarithmResolverController.java
+++ src/main/java/com/example/LogarithmResolver/controllers/LogarithmResolverController.java
@@ -3,11 +3,14 @@ package com.example.LogarithmResolver.controllers;
 import com.example.LogarithmResolver.model.InputParam;
 import com.example.LogarithmResolver.model.ResponseMessage;
 import com.example.LogarithmResolver.services.interfaces.LogarithmResolverService;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+@Slf4j
 @RestController
 public class LogarithmResolverController {
 
@@ -16,13 +19,13 @@ public class LogarithmResolverController {
 
     @GetMapping("/logarithm")
     public ResponseMessage logarithm(@RequestParam(name="base") String base, @RequestParam(name="number") String number) {
-        System.out.println("base = " + base + "number = " + number);
+        log.info("base = " + base + " number = " + number);
         double inputNumber = Double.parseDouble(number);
         double inputBase = Double.parseDouble(base);
         InputParam inputParam = new InputParam(inputBase, inputNumber);
         Double result = logarithmService.resolveLogarithm(inputParam);
 
-        System.out.println("result = " + result);
+        log.info("result = " + result);
         return new ResponseMessage(1,"Success", inputParam, result);
     }
 }
